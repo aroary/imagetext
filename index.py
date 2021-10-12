8e728a0b4f8246b39158fc9143a02be862a0aa38
aroary <meanchemshkedi@gmail.com>
2021-10-11 22:44:42 -0400

    remove downloaded image

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
 import pathlib
 import shutil
 import requests
+import os
 import webbrowser
 from PIL import Image
 from pytesseract import pytesseract
@@ -55,3 +56,5 @@ for i in urls["http"]:
         print("Opening...")
 
 print(f"Data found:\n{text}")
+
+os.remove(file)
